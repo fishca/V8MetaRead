d7f116a2d6fcc03674973e9688fd1304fd218e41
fishca <sergrud@mail.ru>
2020-08-30 23:19:06 +0300

    Продолжение переноса

--- ConfigStorage.pas
+++ ConfigStorage.pas
@@ -14,10 +14,57 @@ type
 //	void* addin;
 //};
 
+table_file_packed = (
+  tfp_unknown,
+  tfp_no,
+  tfp_yes
+);
+
 //PConfigFile = ^ConfigFile;
 ConfigFile = record
   str: TStream;
   addin : Pointer;
+end;
+
+//---------------------------------------------------------------------------
+// Структура адреса файла таблицы-контейнера файлов
+table_blob_file = record
+  blob_start : Integer;
+  blob_length : Integer;
+end;
+
+//---------------------------------------------------------------------------
+// Структура записи таблицы контейнера файлов
+table_rec = record
+  name: string;
+  addr: table_blob_file;
+  partno: Integer;
+  ft_create: FILETIME;
+  ft_modify: FILETIME;
+end;
+
+table = class; // надо определить
+
+//---------------------------------------------------------------------------
+// Структура файла таблицы контейнера файлов
+table_file = class
+public
+  t: table;
+  name: string;
+  maxpartno: Integer;
+  addr: TArray<table_blob_file>;
+  ft_create: FILETIME;
+  ft_modify: FILETIME;
+  constructor Create(_t: table; _name: string; _maxpartno: Integer);
+  destructor Destroy;
+end;
+
+field = class
+
+end;
+
+table = class
+
 end;
 
 //---------------------------------------------------------------------------
@@ -120,6 +167,49 @@ public
 
 end;
 
+//---------------------------------------------------------------------------
+// Класс таблицы контейнера файлов (CONFIG, CONFIGSAVE, PARAMS, FILES, CONFICAS, CONFICASSAVE)
+TableFiles = class
+private
+  tab: table;
+  allfiles: TDictionary<string,table_file>;
+  rec: Char;
+  ready: Boolean;
+  function test_table(): Boolean;
+public
+  constructor Create(t: table);
+  destructor Destroy();
+  function getready(): Boolean;
+  function getfile(name: string): table_file;
+  function gettable(): table;
+  property files: TDictionary<string,table_file> read allfiles;
+end;
+
+//---------------------------------------------------------------------------
+// Структура файла контейнера файлов
+container_file = class
+public
+  _file : table_file;
+  name: string;      // Приведенное имя (очищенное от динамического обновления)
+  stream,
+  rstream: TStream;  // raw stream (нераспакованный поток)
+  fname: string;     // Имя временого файла, содержащего stream
+  rfname: string;    // Имя временого файла, содержащего rstream
+  cat: v8catalog;
+  _packed: table_file_packed;
+
+  // Номер (индекс) динамического обновления (0, 1 и т.д.).
+  // Если без динамического обновления, то -1, если UID динамического обновления
+  // не найден, то -2. Для пропускаемых файлов -3.
+  dynno: Integer;
+  temppath : string;
+  constructor Create(_f: table_file; _name: string);
+  destructor Destroy;
+  function open(): Boolean;
+  function ropen(): Boolean;
+  procedure close();
+  function isPacked(): Boolean;
+end;
 
 implementation
 
@@ -237,8 +327,11 @@ end;
 { ConfigStorageCFFile }
 
 procedure ConfigStorageCFFile.close(cf: ConfigFile);
+var
+  f : v8file;
 begin
-
+  f := v8file(cf.addin);
+  f.Close;
 end;
 
 constructor ConfigStorageCFFile.Create(fname: string);
@@ -255,13 +348,37 @@ begin
 end;
 
 function ConfigStorageCFFile.fileexists(const path: string): Boolean;
+var
+  f : v8file;
+  i: Integer;
+  fname: string;
 begin
+	// По сути, проверяется существование только каталога (файла верхнего уровня)
+	// Это неправильно для формата 8.0 с файлом каталогом metadata. Но метод fileexists используется только для внешних файлов,
+	// поэтому такой проверки достаточно
 
+  if not cat.IsOpen then
+  begin
+    Result := False;
+    Exit;
+  end;
+
+  fname := TStringBuilder(path).Replace('/', '\').ToString;
+  i := Pos('\', fname);
+  if i <> 0 then
+    fname := fname.Substring(1, i - 1);
+  f := cat.GetFile(fname);
+  if not Assigned(f) then
+  begin
+    Result := False;
+    Exit;
+  end;
+  Result := True;
 end;
 
 function ConfigStorageCFFile.presentation: string;
 begin
-
+  Result := filename;
 end;
 
 function ConfigStorageCFFile.readfile(const path: string): ConfigFile;
@@ -306,7 +423,318 @@ end;
 
 function ConfigStorageCFFile.writefile(const path: string;
   str: TStream): Boolean;
+var
+  f: v8file;
+  c: v8catalog;
+  i, j,k: Integer;
+  fname: string;
 begin
+
+  if not cat.IsOpen then
+  begin
+    Result := False;
+    Exit;
+  end;
+
+  fname := TStringBuilder(path).Replace('/', '\').ToString;
+  c := cat;
+
+  j := Pos('\', fname);
+  k := Pos('\', fname);
+  for i := j to k do
+  begin
+    c := c.Create_Catalog(fname.Substring(1, i - 1));
+    fname := fname.Substring(i + 1, fname.Length - i);
+  end;
+  f := c.Create_File(fname);
+  f.Write(str);
+
+  Result := True;
+
+end;
+
+{ table_file }
+
+constructor table_file.Create(_t: table; _name: string; _maxpartno: Integer);
+var
+  i: Integer;
+begin
+  t := _t;
+  name := _name;
+  maxpartno := _maxpartno;
+  SetLength(addr, maxpartno);
+  for i := 0 to maxpartno do
+  begin
+    addr[i].blob_start := 0;
+    addr[i].blob_length := 0;
+  end;
+  ft_create.dwLowDateTime  := 0;
+  ft_create.dwHighDateTime := 0;
+
+  ft_modify.dwLowDateTime  := 0;
+  ft_modify.dwHighDateTime := 0;
+end;
+
+destructor table_file.Destroy;
+begin
+
+  inherited Destroy;
+end;
+
+{ TableFiles }
+
+constructor TableFiles.Create(t: table);
+var
+  filename, f, partno: field;
+  start: ^Integer;
+  length: ^Integer;
+  create: ^AnsiChar;
+  modify: ^AnsiChar;
+  i,j : Integer;
+  s: string;
+  tr: table_rec;
+  ptr: ^table_rec;
+  allrec: TArray<table_rec>;
+  maxpartnos: TDictionary<string,Integer>;
+  tf: table_file;
+
+begin
+  tab := t;
+  ready := test_table;
+  if not ready then
+    Exit;
+
+end;
+
+destructor TableFiles.Destroy;
+begin
+  allfiles.Free;
+end;
+
+function TableFiles.getfile(name: string): table_file;
+
+begin
+  if not allfiles.TryGetValue(name.UpperCase(name), Result) then
+    Result := nil;
+end;
+
+function TableFiles.getready: Boolean;
+begin
+  Result := ready;
+end;
+
+function TableFiles.gettable: table;
+begin
+  Result := tab;
+end;
+
+function TableFiles.test_table: Boolean;
+begin
+
+end;
+
+{ container_file }
+
+procedure container_file.close;
+begin
+  cat.Free;
+  cat := nil;
+  if stream <> rstream then
+  begin
+    stream.Free;
+    rstream.Free;
+  end
+  else
+    stream.Free;
+
+  if not fname.IsEmpty then
+    DeleteFile(PWideChar(fname));
+  if not rfname.IsEmpty then
+    DeleteFile(PWideChar(rfname));
+
+  fname := '';
+  rfname := '';
+
+end;
+
+constructor container_file.Create(_f: table_file; _name: string);
+begin
+  _file := _f;
+  name := _name;
+  stream := nil;
+  rstream := nil;
+  fname := '';
+  rfname := '';
+  cat := nil;
+  _packed := tfp_unknown;
+  dynno := -3;
+end;
+
+destructor container_file.Destroy;
+begin
+  close;
+end;
+
+function container_file.isPacked: Boolean;
+begin
+//	int i;
+//	String ext;
+//
+//	if(name.CompareIC(L"DynamicallyUpdated") == 0) return false;
+//	if(name.CompareIC(L"SprScndInfo") == 0) return false;
+//	if(name.CompareIC(L"DBNamesVersion") == 0) return false;
+//	if(name.CompareIC(L"siVersions") == 0) return false;
+//	if(name.UpperCase().Pos(L"FO_VERSION") > 0) return false;
+//	if(name[1] == L'{') return false;
+//	i = name.LastDelimiter(spoint);
+//	if(i)
+//	{
+//		ext = name.SubString(i + 1, name.Length() - i).UpperCase();
+//		if(ext.Compare(L"INF") == 0) return false;
+//		if(ext.Compare(L"PFL") == 0) return false;
+//	}
+//	return true;
+  Result := True;
+end;
+
+function container_file.open: Boolean;
+var
+  ts: TStream;
+  tn: string;
+  tempfile: string;
+  i, maxpartno: Integer;
+  t: table;
+  addr: TArray<table_blob_file>;
+begin
+  if Assigned(stream) then
+  begin
+    stream.Seek(0, soBeginning);
+    Result := True;
+    Exit;
+  end;
+  t := _file.t;
+  addr := _file.addr;
+  maxpartno := _file.maxpartno;
+  if maxpartno > 0 then
+  begin
+    GetTempFileName(PWideChar(temppath), 'awa', 0, PWideChar(tempfile));
+    tn := tempfile;
+    stream := TFileStream.Create(tn, fmCreate);
+    fname := tn;
+  end
+  else
+    stream := TMemoryStream.Create;
+  if _packed = tfp_unknown then
+  begin
+    if isPacked then
+      _packed := tfp_yes
+    else
+      _packed := tfp_no;
+  end;
+
+  if Assigned(rstream) then
+  begin
+    if _packed = tfp_yes then
+      ts := rstream
+    else
+      begin
+        stream := rstream;
+        stream.Seek(0, soBeginning);
+        Result := True;
+        Exit;
+      end;
+
+  end
+  else
+  begin
+    if _packed = tfp_yes then
+    begin
+      if maxpartno > 0 then
+      begin
+        GetTempFileName(PWideChar(temppath), 'awa', 0, PWideChar(tempfile));
+        tn := tempfile;
+        ts := TFileStream.Create(tn, fmCreate);
+      end
+      else
+        ts := TMemoryStream.Create;
+    end
+    else
+      ts := stream;
+    for i := 0 to maxpartno do
+    begin
+      // t.readBlob(ts, addr[i].blob_start, addr[i].blob_length, false); // Надо доделать!!!!!!!!
+    end;
+  end;
+
+  if _packed = tfp_yes then
+  begin
+    ts.Seek(0, soBeginning);
+    ZInflateStream(ts, stream);
+    if not Assigned(rstream) then
+    begin
+      ts.Free;
+      DeleteFile(PWideChar(tn));
+    end;
+  end;
+
+  stream.Seek(0, soBeginning);
+  Result := True;
+
+end;
+
+function container_file.ropen: Boolean;
+var
+  //ts: TStream;
+  //tn: string;
+  tempfile: string;
+  i, maxpartno: Integer;
+  t: table;
+  addr: TArray<table_blob_file>;
+begin
+  if Assigned(rstream) then
+  begin
+    rstream.Seek(0, soBeginning);
+    Result := True;
+    Exit;
+  end;
+
+  t := _file.t;
+  addr := _file.addr;
+
+  maxpartno := _file.maxpartno;
+  if _packed = tfp_unknown then
+  begin
+    if isPacked then
+      _packed := tfp_yes
+    else
+      _packed := tfp_no;
+  end;
+
+  if (_packed = tfp_no) and Assigned(stream) then
+  begin
+    rstream := stream;
+    rstream.Seek(0, soFromBeginning);
+    Result := True;
+    Exit;
+  end;
+
+  if maxpartno > 0 then
+  begin
+    GetTempFileName(PWideChar(temppath), 'awa', 0, PWideChar(tempfile));
+    rfname := tempfile;
+    rstream := TFileStream.Create(rfname, fmCreate);
+  end
+  else
+    rstream := TMemoryStream.Create;
+
+  for i := 0 to maxpartno do
+  begin
+      // t.readBlob(ts, addr[i].blob_start, addr[i].blob_length, false); // Надо доделать!!!!!!!!
+  end;
+
+  rstream.Seek(0, soBeginning);
+
+  Result := True;
 
 end;
 
--- MetaData.pas
+++ MetaData.pas
@@ -2,6 +2,293 @@ unit MetaData;
 
 interface
 
+uses System.Classes, System.IOUtils, System.SysUtils, Winapi.Windows, NodeTypes,
+     System.RegularExpressions, System.Syncobjs, System.Generics.Collections,
+     System.WideStrUtils, ZZlib, APIcfBase, Parse_tree;
+
+
+type
+//---------------------------------------------------------------------------
+// Версии контейнера 1С
+// значения версий должны обязательно располагаться по возрастанию, чтобы можно было сравнивать версии на >, < и =
+ContainerVer = (
+	cv_2_0   = 1,
+	cv_5_0   = 2,
+	cv_6_0   = 3,
+	cv_106_0 = 4,
+	cv_200_0 = 5,
+	cv_202_2 = 6,
+	cv_216_0 = 7
+
+);
+
+//---------------------------------------------------------------------------
+// Версии 1С
+// значения версий должны обязательно располагаться по возрастанию, чтобы можно было сравнивать версии на >, < и =
+Version1C = (
+	v1C_min    = 0,
+	v1C_8_0    = 1,
+	v1C_8_1    = 2,
+	v1C_8_2    = 3,
+	v1C_8_2_14 = 4,
+	v1C_8_3_1  = 5,
+	v1C_8_3_2  = 6,
+	v1C_8_3_3  = 7,
+	v1C_8_3_4  = 8,
+	v1C_8_3_5  = 9,
+	v1C_8_3_6  = 10
+
+);
+
+//---------------------------------------------------------------------------
+// Способы выгрузки
+ExportType =
+(
+	et_default = 0,
+	et_catalog = 1,
+	et_file    = 2
+);
+
+//---------------------------------------------------------------------------
+// Базовый класс метаданных 1С
+MetaBase = class
+protected
+  fname: string;
+  fename: string;
+public
+  constructor Create(); overload;
+  constructor Create(_name: string;_ename:string); overload;
+  destructor Destroy;
+  procedure setname(_name: string);
+  procedure setename(_ename: string);
+  property name : string read fname;
+  property ename: string read fename;
+  function getname(english: Boolean = False): string;
+
+end;
+
+MetaType = class;
+
+//---------------------------------------------------------------------------
+// Предопределенное значение метаданных
+MetaValue = class(MetaBase)
+protected
+  owner: MetaType;
+  fvalue: Integer;
+  fvalueUID: TGUID;
+public
+  constructor Create(_owner: MetaType; _name: string; _ename: string; _value: Integer); overload;
+  constructor Create(_owner: MetaType; tr: tree); overload;
+  destructor Destroy;
+  property value    : Integer read fvalue;
+  property valueUID : TGUID   read fvalueUID;
+  function getowner(): MetaType;
+end;
+
+MetaType = class
+
+end;
+
+//---------------------------------------------------------------------------
+// Виды значений по умолчанию
+DefaultValueType =
+(
+	dvt_novalue = 0, // Нет значения по умолчанию
+	dvt_bool    = 1, // Булево
+	dvt_number  = 2, // Число
+	dvt_string  = 3, // Строка
+	dvt_date    = 4, // Дата
+	dvt_undef   = 5, // Неопределено
+	dvt_null    = 6, // Null
+	dvt_type    = 7, // Тип
+	dvt_enum    = 8 // Значение системного перечисления
+);
+
+dv_val = record
+  case _dv_val : 1..6 of
+    1: (dv_bool   : Boolean);
+    2: (dv_number : Integer);
+    3: (dv_string : PAnsiChar);
+    4: (dv_date   : array[0..6] of AnsiChar);
+    5: (dv_type   : MetaType);
+    6: (dv_enum   : MetaValue);
+end;
+
+
+T_Class = class;
+
+//---------------------------------------------------------------------------
+// Свойство метаданных
+MetaProperty = class(MetaBase)
+protected
+  ftypes: TArray<MetaType>;
+  fstypes: TArray<string>;
+  owner: MetaType;
+  fpredefined: Boolean;
+  fexporttype : ExportType;
+  f_class : T_Class;
+public
+  defaultvaluetype : DefaultValueType;
+  _dv_val: dv_val;
+  constructor Create(_owner: MetaType; _name: string; _ename: string); overload;
+  constructor Create(_owner: MetaType; tr: tree); overload;
+  destructor Destroy;
+  procedure filltypes;
+  property types: TArray<MetaType> read ftypes;
+  property predefined: Boolean read fpredefined;
+  property exporttype: ExportType read fexporttype;
+  property _class: T_Class read f_class;
+  function getowner(): MetaType;
+end;
+
+T_Class = class
+
+end;
+
+
 implementation
+
+
+
+{ MetaBase }
+
+constructor MetaBase.Create(_name, _ename: string);
+begin
+  _name := _name;
+  _ename := _ename;
+end;
+
+constructor MetaBase.Create;
+begin
+  inherited;
+end;
+
+destructor MetaBase.Destroy;
+begin
+  inherited;
+end;
+
+function MetaBase.getname(english: Boolean): string;
+begin
+  if english then
+    Result := fename
+  else
+    Result := fname;
+
+end;
+
+procedure MetaBase.setename(_ename: string);
+begin
+  fename := _ename;
+end;
+
+procedure MetaBase.setname(_name: string);
+begin
+  fname := _name;
+end;
+
+{ MetaValue }
+
+constructor MetaValue.Create(_owner: MetaType; _name, _ename: string;
+  _value: Integer);
+begin
+  inherited Create(_name, _ename);
+  owner := _owner;
+  fvalue := _value;
+end;
+
+constructor MetaValue.Create(_owner: MetaType; tr: tree);
+var
+  tt: tree;
+begin
+  tt := tr.get_first;
+  fname := tt.get_value;
+
+  tt := tt.get_next;
+  fename := tt.get_value;
+
+  tt := tt.get_next;
+  fvalue := tt.get_value.ToInteger();
+
+  tt := tt.get_next;
+  // string_to_GUID(tt.get_value, fvalueUID); Надо доделать!!!!!!!!!!!!!!!
+
+end;
+
+destructor MetaValue.Destroy;
+begin
+  inherited;
+end;
+
+function MetaValue.getowner: MetaType;
+begin
+  Result := owner;
+end;
+
+{ MetaProperty }
+
+constructor MetaProperty.Create(_owner: MetaType; _name, _ename: string);
+begin
+  inherited Create(_name, _ename);
+  owner := _owner;
+end;
+
+constructor MetaProperty.Create(_owner: MetaType; tr: tree);
+var
+  tt, t : tree;
+  num, i : Integer;
+  guid : TGUID;
+begin
+  tt := tt.get_first;
+  fname := tt.get_value;
+
+  tt := tt.get_next;
+  fename := tt.get_value;
+
+  tt := tt.get_next;
+  if tt.get_value.CompareTo('1') = 0 then
+    fpredefined :=  True
+  else
+    fpredefined :=  False;
+
+  tt := tt.get_next;
+  // string_to_GUID(tt.get_value, guid); // Надо доделать!!!!!!!!!!!!
+  // f_class := T_Class.getclass(guid);    // Надо доделать!!!!!!!!!!!!
+
+  // Типы
+  tt := tt.get_next;
+  t := tt.get_first;
+  num := t.get_value.ToInteger();
+  SetLength(fstypes, num);
+  for i := 0 to num do
+  begin
+    t := t.get_next;
+    fstypes[i] := t.get_value;
+  end;
+
+  defaultvaluetype := dvt_novalue;
+end;
+
+destructor MetaProperty.Destroy;
+begin
+  inherited Destroy;
+end;
+
+procedure MetaProperty.filltypes;
+var
+   i : Integer;
+begin
+  SetLength(ftypes, Length(fstypes));
+  for i := 0 to Length(fstypes) do
+  begin
+    //ftypes[i] := owner.TypeSet.getTypeByName(i)); // Надо доделать!!!!!!!!!!!!
+  end;
+
+end;
+
+function MetaProperty.getowner: MetaType;
+begin
+  Result := owner;
+end;
 
 end.
